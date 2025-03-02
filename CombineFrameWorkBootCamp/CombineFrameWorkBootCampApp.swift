ae5334edfcfd5bf20be6eb7f72da01c5ccdf8995
Adarsh Ranjan <adarsh.ranjan@roro.io>
2025-03-02 15:38:54 +0530

    Initial Commit

---
+++ CombineFrameWorkBootCamp/CombineFrameWorkBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFrameWorkBootCampApp.swift
+//  CombineFrameWorkBootCamp
+//
+//  Created by Adarsh Ranjan on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineFrameWorkBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineFrameWorkBootCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombineFrameWorkBootCamp
+//
+//  Created by Adarsh Ranjan on 02/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
